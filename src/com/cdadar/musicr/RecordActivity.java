a243156e300961243eb8f01345f9c938c93c7f6b
Alok Thapa <alok@fishinmyhead.com>
2010-11-24 01:20:34 -0600

    added permission for wake_lock

--- src/com/cdadar/musicr/RecordActivity.java
+++ src/com/cdadar/musicr/RecordActivity.java
@@ -14,16 +14,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.os.PowerManager;
 import android.content.Context;
-
 import com.cdadar.musicr.work.*;
 
 public class RecordActivity extends Activity {
-	Project p = null;	
-	RehearsalAudioRecorder rec = null;
-	MediaPlayer mp = null;
+    Project p = null;	
+    RehearsalAudioRecorder rec = null;
+    MediaPlayer mp = null;
 	
-	public void onCreate(Bundle savedInstanceState) 
-	{
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.record);
         TextView tv = (TextView)findViewById(R.id.txtrecordproject);
@@ -38,108 +37,116 @@ public class RecordActivity extends Activity {
         
 
         findViewById(R.id.btnplay).setOnClickListener(
-        		new View.OnClickListener() {
+						      new View.OnClickListener() {
 					
-					@Override
-					public void onClick(View v) {
-						// TODO Auto-generated method stub
-					    PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-					    PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK);
-					    wl.aquire();
-					    
-						MixWave mix = new MixWave(P.currentProject());
-						mix.mix();
-					    mp = new MediaPlayer();
-					    try{
+							  @Override
+							      public void onClick(View v) {
+							      // TODO Auto-generated method stub
+							      PowerManager pm = (PowerManager) v.getContext().getSystemService(Context.POWER_SERVICE);
+							      PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "musicr");
+							      wl.acquire();
+							      MixWave mix = new MixWave(P.currentProject());
+							      mix.mix();
+							      mp = new MediaPlayer();
+							      try{
 					    	
-						    mp.setDataSource(P.currentProject().getTrackPath("mix"));
-						    mp.prepare();
+								  mp.setDataSource(P.currentProject().getTrackPath("mix"));
+								  mp.prepare();
 					    
 						    
-						// and record!
-        				rec = 
-        					new RehearsalAudioRecorder(
-        							true,   //record uncompressed
-        							AudioSource.MIC, 			//audio source
-        							44100, 						//sample rate
-        							AudioFormat.CHANNEL_CONFIGURATION_MONO,  //mono recording
-        							AudioFormat.ENCODING_PCM_16BIT); // 16 bit recording
-        				rec.setOutputFile(p.getCurrentTrackPath());
-        				rec.prepare();
+								  // and record!
+								  rec = 
+								      new RehearsalAudioRecorder(
+												 true,   //record uncompressed
+												 AudioSource.MIC, 			//audio source
+												 44100, 						//sample rate
+												 AudioFormat.CHANNEL_CONFIGURATION_MONO,  //mono recording
+												 AudioFormat.ENCODING_PCM_16BIT); // 16 bit recording
+								  rec.setOutputFile(p.getCurrentTrackPath());
+								  rec.prepare();
 
-					    mp.start();
+								  mp.start();
 
-        				rec.start();
-        				findViewById(R.id.btnrecord).setEnabled(false);
-        				findViewById(R.id.btnstoprecord).setEnabled(true);
+								  rec.start();
+								  findViewById(R.id.btnrecord).setEnabled(false);
+								  findViewById(R.id.btnstoprecord).setEnabled(true);
 
-					    }
-					    catch(Exception e)
-						{ 
-						Log.e("err", "error in btnplay onclick: "+ e.toString());
-						if (mp !=null)
-						    mp.release();
-						if (rec !=null)
-						    rec.release();
+							      }
+							      catch(Exception e)
+								  { 
+								      Log.e("err", "error in btnplay onclick: "+ e.toString());
+								      if (mp !=null)
+									  mp.release();
+								      if (rec !=null)
+									  rec.release();
 
-					    }
-					    finally
-						{
-						    wl.release();
+								  }
+							      finally
+								  {
+								      wl.release();
 						    
 
-						}
+								  }
 
-					}
-				});
+							  }
+						      });
         
         findViewById(R.id.btnrecord).setOnClickListener(
-        		new View.OnClickListener() {
-        			public void onClick(View v) {
-        				rec = 
-        					new RehearsalAudioRecorder(
-        							true,   //record uncompressed
-        							AudioSource.MIC, 			//audio source
-        							44100, 						//sample rate
-        							AudioFormat.CHANNEL_CONFIGURATION_MONO,  //mono recording
-        							AudioFormat.ENCODING_PCM_16BIT); // 16 bit recording
-        				rec.setOutputFile(p.getCurrentTrackPath());
-        				rec.prepare();
-        				rec.start();
-        				findViewById(R.id.btnrecord).setEnabled(false);
-        				findViewById(R.id.btnstoprecord).setEnabled(true);
-   		}});
+							new View.OnClickListener() {
+							    public void onClick(View v) {
+
+							      PowerManager pm = (PowerManager) v.getContext().getSystemService(Context.POWER_SERVICE);
+							      PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "musicr");
+							      wl.acquire();
+
+								rec = 
+								    new RehearsalAudioRecorder(
+											       true,   //record uncompressed
+											       AudioSource.MIC, 			//audio source
+											       44100, 						//sample rate
+											       AudioFormat.CHANNEL_CONFIGURATION_MONO,  //mono recording
+											       AudioFormat.ENCODING_PCM_16BIT); // 16 bit recording
+								rec.setOutputFile(p.getCurrentTrackPath());
+								rec.prepare();
+								rec.start();
+								findViewById(R.id.btnrecord).setEnabled(false);
+								findViewById(R.id.btnstoprecord).setEnabled(true);
+								wl.release();
+								
+							    }});
+
+
         
     	findViewById(R.id.btnsettings ).setOnClickListener(    	
-    			new View.OnClickListener() {
-					public void onClick(View v) {
-			            Intent myIntent = new Intent(v.getContext(), SettingsActivity.class);
-			            startActivityForResult(myIntent, 0);
-					}
-				});
+							   new View.OnClickListener() {
+							       public void onClick(View v) {
+								   Intent myIntent = new Intent(v.getContext(), SettingsActivity.class);
+								   startActivityForResult(myIntent, 0);
+							       }
+							   });
     	
         findViewById(R.id.btnstoprecord).setOnClickListener(
-        		new View.OnClickListener() {
-        			public void onClick(View v) {
+							    new View.OnClickListener() {
+								public void onClick(View v) {
     			
-        				if (mp != null)
-        				{
-        					mp.stop();
-        					mp.release();
-        				}
-        				if (rec != null)
-        				{
-        					rec.stop();
-        					rec.release();
-            				findViewById(R.id.btnrecord).setEnabled(true);
-            		        findViewById(R.id.btnstoprecord).setEnabled(false);
-        				}
+								    if (mp != null)
+									{
+									    mp.stop();
+									    mp.release();
+									}
+								    if (rec != null)
+									{
+									    rec.stop();
+									    rec.release();
+									    findViewById(R.id.btnrecord).setEnabled(true);
+									    findViewById(R.id.btnstoprecord).setEnabled(false);
+									}
 
-        			}});
+								}});
         
         		
         
-	}
+    }
 	
 	
 
