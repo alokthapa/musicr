ac7d4376dfdbf7a8482bb2086c4ae06854fb59b3
Alok Thapa <alok@fishinmyhead.com>
2010-11-24 01:00:31 -0600

    added some new files

---
+++ src/com/cdadar/musicr/OpenActivity.java
@@ -0,0 +1,21 @@
+package com.cdadar.musicr;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OpenActivity extends Activity{
+	
+	
+	
+	public void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.open);
+        
+
+		
+		
+	}
+
+}
